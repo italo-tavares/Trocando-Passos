6e36b3b2504099b1264992576224e4ef9d324ef7
italo-tavares <156615667+italo-tavares@users.noreply.github.com>
2024-09-19 22:41:51 -0300

    Update empre.js

--- empre.js
+++ empre.js
@@ -1,4 +1,1 @@
-// Script básico para interações e animações futuras
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Site da Tênis em Movimento carregado com sucesso!');
-});
+
