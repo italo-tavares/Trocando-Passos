4b21cd8ddfc93eda93a0c7e5f9f9798166882cb4
italo-tavares <itavares119@gmail.com>
2024-09-19 22:35:52 -0300

    Initial commit

---
+++ empre.js
@@ -0,0 +1,4 @@
+// Script básico para interações e animações futuras
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Site da Tênis em Movimento carregado com sucesso!');
+});
